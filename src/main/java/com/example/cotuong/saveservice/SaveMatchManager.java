8bdbc60d9cddf254770129de755769d207f58070
chun2005 <hoangquangnghia123@gmail.com>
2025-04-10 22:11:51 +0700

    add CryptoUtil(encrypt)

--- src/main/java/com/example/cotuong/saveservice/SaveMatchManager.java
+++ src/main/java/com/example/cotuong/saveservice/SaveMatchManager.java
@@ -4,25 +4,27 @@ import com.example.cotuong.chesslogic.gamestate.GameState;
 import com.example.cotuong.chesslogic.gamestate.GameState2P;
 import com.example.cotuong.chesslogic.gamestate.GameStateAI;
 import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+import java.io.File;
 
 
 public class SaveMatchManager {
-//    RuntimeTypeAdapterFactory<GameState> factory = RuntimeTypeAdapterFactory
-//            .of(GameState.class, "type")  // "type" sẽ là trường lưu tên class con
-//            .registerSubtype(GameState2P.class, "2P")
-//            .registerSubtype(GameStateAI.class, "AI");
-    private static final Gson gson = new Gson();
-    public void save(GameState gameState){
-
-
-    }
-    public void load(){
-
-    }
-//    public GameStateForLoad fromSave(GameStateForSave gameStateForSave){
+    private static final RuntimeTypeAdapterFactory<GameState> factory = RuntimeTypeAdapterFactory
+            .of(GameState.class, "type")
+            .registerSubtype(GameState2P.class, "2P")
+            .registerSubtype(GameStateAI.class, "AI");
+    private static final Gson gson = new GsonBuilder()
+            .registerTypeAdapterFactory(factory)
+            .setPrettyPrinting()
+            .create();
+//    public void save(GameState gameState){
+//
 //
 //    }
-//    public GameStateForSave toSave(GameState gameState){
+//    public GameState load(String filePath){
 //
 //    }
+
 }
+
