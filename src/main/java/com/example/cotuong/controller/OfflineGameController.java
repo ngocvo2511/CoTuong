8cb29e32764aca2d18f3d87efc8002a181428b42
Minhoang2445 <vqminhhoang2445@gmail.com>
2025-06-01 00:51:17 +0700

    add back to the game and next button

--- src/main/java/com/example/cotuong/controller/OfflineGameController.java
+++ src/main/java/com/example/cotuong/controller/OfflineGameController.java
@@ -54,6 +54,10 @@ public class OfflineGameController {
     @FXML
     private Button saveButton;
     @FXML
+    private Button playButton;
+    @FXML
+    private Button nextButton;
+    @FXML
     private VBox controlButtons;
     @FXML
     private AnchorPane rootPane;
